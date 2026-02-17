f56f8b6a47f2dde717413c4e19f080f8d7ef22ab
eshansinghal14 <eshansinghal05@gmail.com>
2026-02-17 16:41:04 -0500

    generate weezl tokenizer

--- gpt.py
+++ gpt.py
@@ -26,6 +26,88 @@ dev = (
 )
 maskid, padid = None, None
 
+
+class WeezlTokenizerTSV:
+    def __init__(self, path):
+        self.path = path
+        self.pattern_to_code, self.code_to_pattern = self._load_dictionary(path)
+        self._base_vocab = max(self.code_to_pattern.keys(), default=0) + 1
+        self.pad_id = self._base_vocab
+        self.unk_id = self._base_vocab + 1
+        self.mask_id = self._base_vocab + 2
+
+    def vocab_size(self):
+        return self._base_vocab + 3
+
+    def encode(self, text):
+        chars = list(text)
+        tokens = []
+        i = 0
+        while i < len(chars):
+            current = ''
+            best_code = None
+            best_len = 0
+            j = i
+            while j < len(chars):
+                current += chars[j]
+                code = self.pattern_to_code.get(current)
+                if code is not None:
+                    best_code = code
+                    best_len = j - i + 1
+                    j += 1
+                else:
+                    break
+
+            if best_code is not None:
+                tokens.append(best_code)
+                i += best_len
+            else:
+                tokens.append(self.unk_id)
+                i += 1
+        return tokens
+
+    def decode(self, ids):
+        out = []
+        for tid in ids:
+            if tid == self.pad_id:
+                continue
+            if tid == self.unk_id:
+                out.append('�')
+                continue
+            if tid == self.mask_id:
+                out.append('[MASK]')
+                continue
+            pat = self.code_to_pattern.get(int(tid))
+            if pat is not None:
+                out.append(pat)
+            else:
+                out.append('�')
+        return ''.join(out)
+
+    @staticmethod
+    def _unescape(s):
+        return (s.replace('\\\\', '\x00').replace('\\t', '\t')
+                .replace('\\n', '\n').replace('\\r', '\r')
+                .replace('\\0', '\0').replace('\x00', '\\'))
+
+    @classmethod
+    def _load_dictionary(cls, path):
+        pattern_to_code = {}
+        code_to_pattern = {0: ''}
+        with open(path, 'r', encoding='utf-8') as f:
+            for line in f:
+                line = line.rstrip('\n')
+                if not line or line.startswith('#'):
+                    continue
+                parts = line.split('\t', 3)
+                if len(parts) < 4:
+                    continue
+                code = int(parts[0])
+                pattern = cls._unescape(parts[3])
+                pattern_to_code[pattern] = code
+                code_to_pattern[code] = pattern
+        return pattern_to_code, code_to_pattern
+
 def setup(s):
     th.manual_seed(s)
     random.seed(s)
@@ -179,19 +261,48 @@ class gpt_t(nn.Module):
             r = th.cat((r, xp), dim=1)
         return r
 
-def get_data(f='/tmp/10.txt.utf-8', m=0, reset=False):
+def get_data(
+    f='/tmp/10.txt.utf-8',
+    m=0,
+    reset=False,
+    tokenizer='sp',
+    weezl_dict_path=None,
+    vocab_size=1024,
+):
     global maskid, padid
-
+ 
+    if tokenizer == 'weezl':
+        if weezl_dict_path is None:
+            raise ValueError('weezl_dict_path is required when tokenizer="weezl"')
+        tok = WeezlTokenizerTSV(weezl_dict_path)
+        padid, maskid = tok.pad_id, tok.mask_id
+ 
+        cache_path = f"d_weezl_{os.path.basename(weezl_dict_path)}.p"
+        if not os.path.exists(cache_path) or reset:
+            d = {}
+            d['vdim'] = tok.vocab_size()
+            d['x'] = []
+            x = open(f, 'r', encoding='utf-8', errors='replace').read()
+            x = nltk.sent_tokenize(x)
+            for _ in range(max(1, m)):
+                for sent in x:
+                    d['x'] += tok.encode(sent)
+            pickle.dump(d, open(cache_path, 'wb'))
+        else:
+            d = pickle.load(open(cache_path, 'rb'))
+        return d
+ 
     if not os.path.exists('tok.model') or reset:
         sp.SentencePieceTrainer.train(input=f,
                                       model_prefix='tok',
                                       model_type='bpe',
-                                      vocab_size=1024,
+                                      vocab_size=vocab_size,
                                       normalization_rule_name='nmt_nfkc_cf')
                                       #user_defined_symbols=['[mask]','[pad]'])
-
+ 
     s = sp.SentencePieceProcessor(model_file='tok.model')
-    maskid, padid = s.piece_to_id('[mask]'), s.piece_to_id('[pad]')
+    padid = 0
+    maskid = None
     if not os.path.exists('d.p') or reset:
         d = {}
         d['vdim'] = s.vocab_size()
@@ -212,7 +323,7 @@ def get_batch(ds, b, T, overfit=False):
     N = len(ds['x'])
     ns = th.randint(N, (b,))
     if overfit:
-        ns=th.arange(b)
+        ns = th.arange(b)
 
     x = (padid + th.zeros(b, T)).long()
     y = (padid + th.zeros(b, T)).long()
@@ -237,10 +348,17 @@ def main(seed=42,
          nblock=2,
          generate=False,
          reset=False,
-         data_file='jungle_book.txt'):
+         data_file='jungle_book.txt',
+         tokenizer='sp',
+         weezl_dict_path=None,
+         vocab_size=1024):
 
-    ds = get_data(data_file, reset=reset)
-    s = sp.SentencePieceProcessor(model_file='tok.model')
+    ds = get_data(data_file, reset=reset, tokenizer=tokenizer, weezl_dict_path=weezl_dict_path, vocab_size=vocab_size)
+    tok = None
+    if tokenizer == 'sp':
+        tok = sp.SentencePieceProcessor(model_file='tok.model')
+    elif tokenizer == 'weezl':
+        tok = WeezlTokenizerTSV(weezl_dict_path)
     vdim = ds['vdim']
     m = gpt_t(nblock=nblock,
               vdim=vdim, hdim=hdim, T=T,
@@ -262,8 +380,12 @@ def main(seed=42,
         x, _ = get_batch(ds, bsz, T)
         xp = m.generate(x[:,:T//8], T, temp=0, cache=cache)
         for i in range(bsz):
-            print('x: ', s.decode(x[i].tolist()))
-            print('xp: ', s.decode(xp[i].tolist()))
+            if tokenizer == 'sp':
+                print('x: ', tok.decode(x[i].tolist()))
+                print('xp: ', tok.decode(xp[i].tolist()))
+            else:
+                print('x: ', tok.decode(x[i].tolist()))
+                print('xp: ', tok.decode(xp[i].tolist()))
             print()
         pickle.dump(cache, open('cache.p', 'wb'))
 
@@ -293,8 +415,12 @@ def main(seed=42,
                     xp = m.generate(x[:,:T//2], T, temp=0, cache=cache)
                     #print('x: ', x[0].tolist())
                     #print('xp: ', xp[0].tolist())
-                    print('x: ', s.decode(x[0].tolist()))
-                    print('xp: ', s.decode(xp[0].tolist()))
+                    if tokenizer == 'sp':
+                        print('x: ', tok.decode(x[0].tolist()))
+                        print('xp: ', tok.decode(xp[0].tolist()))
+                    else:
+                        print('x: ', tok.decode(x[0].tolist()))
+                        print('xp: ', tok.decode(xp[0].tolist()))
 
 
 if __name__=='__main__':
